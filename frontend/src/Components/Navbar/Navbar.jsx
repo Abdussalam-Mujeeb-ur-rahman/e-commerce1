d11954c180a31296b4103adbdea158220932e349
Abdussalam-Mujeeb-ur-rahman <mjbabdussalam@gmail.com>
2024-06-18 11:35:13 +0100

    NavBar responsiveness done ✅

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,21 @@
-import React, {useState} from 'react'
+import React, {useContext, useState, useRef} from 'react'
 import './Navbar.css'
-import logo from '../Assets/logo.png'
+import logo from '../Assets/logo.png';
+import navDropdown from '../Assets/dropdown.png'
 import { MdShoppingCart } from "react-icons/md";
 import { Link } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext';
 
 const Navbar = () => {
 
   const [menu, setMenu] = useState("shop");
+  const { getTotalCartItems } = useContext(ShopContext);
+  const menuRef = useRef();
+
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
 
   return (
     <div>
@@ -15,7 +24,8 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
           <p>TMP</p>
         </div>
-        <ul className="nav-menu">
+        <img src={navDropdown} alt="nav-dropdown" className='nav-dropdown' onClick={dropdown_toggle} />
+        <ul ref={menuRef} className="nav-menu">
           <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu === "shop" ? <hr/> : ""}</li>
           <li onClick={()=>{setMenu("men")}}><Link style={{ textDecoration: 'none' }} to='/men'>Men</Link>{menu === "men" ? <hr/> : ""}</li>
           <li onClick={()=>{setMenu("women")}}><Link style={{ textDecoration: 'none' }} to='/women'>Women</Link>{menu === "women" ? <hr/> : ""}</li>
@@ -23,8 +33,8 @@ const Navbar = () => {
         </ul>
         <div className="nav-login-cart">
           <Link style={{ textDecoration: 'none' }} to='/login'><button>Login</button></Link>
-          <Link style={{ textDecoration: 'none' }} to='/cart'><MdShoppingCart className='cart-icon'/></Link>
-          <div className="nav-cart-count">0</div>
+          <Link className='nav-login-cart-image' style={{ textDecoration: 'none' }} to='/cart'><MdShoppingCart className='cart-icon'/></Link>
+          <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
       </div>
     </div>
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -33,7 +33,17 @@ const ShopContextProvider = (props) => {
     return totalAmount;
   }
 
-  const contextValue = { data, cartItems, addToCart, removeFromCart, getTotalCartAmount };
+  const getTotalCartItems = () => {
+    let totalItems = 0;
+    for(const item in cartItems) {
+      if(cartItems[item] > 0) {
+        totalItems += cartItems[item];
+      }
+    }
+    return totalItems;
+  }
+
+  const contextValue = { data, cartItems, addToCart, removeFromCart, getTotalCartAmount, getTotalCartItems };
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
